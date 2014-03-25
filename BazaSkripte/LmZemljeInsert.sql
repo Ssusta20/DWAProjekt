7b86c8c17ea7e7b89265a00524d7a1a03a3c26ed
ssusta20 <ssusta20@vm-0.ssusta20.koding.kd.io>
2014-03-25 11:30:14 +0000

    Dodane skripte za bazu

---
+++ BazaSkripte/LmZemljeInsert.sql
@@ -0,0 +1,477 @@
+INSERT INTO `lm_zemlja` VALUES (1, 'US', 'United States');
+
+INSERT INTO `lm_zemlja` VALUES (2, 'CA', 'Canada');
+
+INSERT INTO `lm_zemlja` VALUES (3, 'AF', 'Afghanistan');
+
+INSERT INTO `lm_zemlja` VALUES (4, 'AL', 'Albania');
+
+INSERT INTO `lm_zemlja` VALUES (5, 'DZ', 'Algeria');
+
+INSERT INTO `lm_zemlja` VALUES (6, 'DS', 'American Samoa');
+
+INSERT INTO `lm_zemlja` VALUES (7, 'AD', 'Andorra');
+
+INSERT INTO `lm_zemlja` VALUES (8, 'AO', 'Angola');
+
+INSERT INTO `lm_zemlja` VALUES (9, 'AI', 'Anguilla');
+
+INSERT INTO `lm_zemlja` VALUES (10, 'AQ', 'Antarctica');
+
+INSERT INTO `lm_zemlja` VALUES (11, 'AG', 'Antigua and/or Barbuda');
+
+INSERT INTO `lm_zemlja` VALUES (12, 'AR', 'Argentina');
+
+INSERT INTO `lm_zemlja` VALUES (13, 'AM', 'Armenia');
+
+INSERT INTO `lm_zemlja` VALUES (14, 'AW', 'Aruba');
+
+INSERT INTO `lm_zemlja` VALUES (15, 'AU', 'Australia');
+
+INSERT INTO `lm_zemlja` VALUES (16, 'AT', 'Austria');
+
+INSERT INTO `lm_zemlja` VALUES (17, 'AZ', 'Azerbaijan');
+
+INSERT INTO `lm_zemlja` VALUES (18, 'BS', 'Bahamas');
+
+INSERT INTO `lm_zemlja` VALUES (19, 'BH', 'Bahrain');
+
+INSERT INTO `lm_zemlja` VALUES (20, 'BD', 'Bangladesh');
+
+INSERT INTO `lm_zemlja` VALUES (21, 'BB', 'Barbados');
+
+INSERT INTO `lm_zemlja` VALUES (22, 'BY', 'Belarus');
+
+INSERT INTO `lm_zemlja` VALUES (23, 'BE', 'Belgium');
+
+INSERT INTO `lm_zemlja` VALUES (24, 'BZ', 'Belize');
+
+INSERT INTO `lm_zemlja` VALUES (25, 'BJ', 'Benin');
+
+INSERT INTO `lm_zemlja` VALUES (26, 'BM', 'Bermuda');
+
+INSERT INTO `lm_zemlja` VALUES (27, 'BT', 'Bhutan');
+
+INSERT INTO `lm_zemlja` VALUES (28, 'BO', 'Bolivia');
+
+INSERT INTO `lm_zemlja` VALUES (29, 'BA', 'Bosnia and Herzegovina');
+
+INSERT INTO `lm_zemlja` VALUES (30, 'BW', 'Botswana');
+
+INSERT INTO `lm_zemlja` VALUES (31, 'BV', 'Bouvet Island');
+
+INSERT INTO `lm_zemlja` VALUES (32, 'BR', 'Brazil');
+
+INSERT INTO `lm_zemlja` VALUES (33, 'IO', 'British lndian Ocean Territory');
+
+INSERT INTO `lm_zemlja` VALUES (34, 'BN', 'Brunei Darussalam');
+
+INSERT INTO `lm_zemlja` VALUES (35, 'BG', 'Bulgaria');
+
+INSERT INTO `lm_zemlja` VALUES (36, 'BF', 'Burkina Faso');
+
+INSERT INTO `lm_zemlja` VALUES (37, 'BI', 'Burundi');
+
+INSERT INTO `lm_zemlja` VALUES (38, 'KH', 'Cambodia');
+
+INSERT INTO `lm_zemlja` VALUES (39, 'CM', 'Cameroon');
+
+INSERT INTO `lm_zemlja` VALUES (40, 'CV', 'Cape Verde');
+
+INSERT INTO `lm_zemlja` VALUES (41, 'KY', 'Cayman Islands');
+
+INSERT INTO `lm_zemlja` VALUES (42, 'CF', 'Central African Republic');
+
+INSERT INTO `lm_zemlja` VALUES (43, 'TD', 'Chad');
+
+INSERT INTO `lm_zemlja` VALUES (44, 'CL', 'Chile');
+
+INSERT INTO `lm_zemlja` VALUES (45, 'CN', 'China');
+
+INSERT INTO `lm_zemlja` VALUES (46, 'CX', 'Christmas Island');
+
+INSERT INTO `lm_zemlja` VALUES (47, 'CC', 'Cocos (Keeling) Islands');
+
+INSERT INTO `lm_zemlja` VALUES (48, 'CO', 'Colombia');
+
+INSERT INTO `lm_zemlja` VALUES (49, 'KM', 'Comoros');
+
+INSERT INTO `lm_zemlja` VALUES (50, 'CG', 'Congo');
+
+INSERT INTO `lm_zemlja` VALUES (51, 'CK', 'Cook Islands');
+
+INSERT INTO `lm_zemlja` VALUES (52, 'CR', 'Costa Rica');
+
+INSERT INTO `lm_zemlja` VALUES (53, 'HR', 'Croatia (Hrvatska)');
+
+INSERT INTO `lm_zemlja` VALUES (54, 'CU', 'Cuba');
+
+INSERT INTO `lm_zemlja` VALUES (55, 'CY', 'Cyprus');
+
+INSERT INTO `lm_zemlja` VALUES (56, 'CZ', 'Czech Republic');
+
+INSERT INTO `lm_zemlja` VALUES (57, 'DK', 'Denmark');
+
+INSERT INTO `lm_zemlja` VALUES (58, 'DJ', 'Djibouti');
+
+INSERT INTO `lm_zemlja` VALUES (59, 'DM', 'Dominica');
+
+INSERT INTO `lm_zemlja` VALUES (60, 'DO', 'Dominican Republic');
+
+INSERT INTO `lm_zemlja` VALUES (61, 'TP', 'East Timor');
+
+INSERT INTO `lm_zemlja` VALUES (62, 'EC', 'Ecudaor');
+
+INSERT INTO `lm_zemlja` VALUES (63, 'EG', 'Egypt');
+
+INSERT INTO `lm_zemlja` VALUES (64, 'SV', 'El Salvador');
+
+INSERT INTO `lm_zemlja` VALUES (65, 'GQ', 'Equatorial Guinea');
+
+INSERT INTO `lm_zemlja` VALUES (66, 'ER', 'Eritrea');
+
+INSERT INTO `lm_zemlja` VALUES (67, 'EE', 'Estonia');
+
+INSERT INTO `lm_zemlja` VALUES (68, 'ET', 'Ethiopia');
+
+INSERT INTO `lm_zemlja` VALUES (69, 'FK', 'Falkland Islands (Malvinas)');
+
+INSERT INTO `lm_zemlja` VALUES (70, 'FO', 'Faroe Islands');
+
+INSERT INTO `lm_zemlja` VALUES (71, 'FJ', 'Fiji');
+
+INSERT INTO `lm_zemlja` VALUES (72, 'FI', 'Finland');
+
+INSERT INTO `lm_zemlja` VALUES (73, 'FR', 'France');
+
+INSERT INTO `lm_zemlja` VALUES (74, 'FX', 'France, Metropolitan');
+
+INSERT INTO `lm_zemlja` VALUES (75, 'GF', 'French Guiana');
+
+INSERT INTO `lm_zemlja` VALUES (76, 'PF', 'French Polynesia');
+
+INSERT INTO `lm_zemlja` VALUES (77, 'TF', 'French Southern Territories');
+
+INSERT INTO `lm_zemlja` VALUES (78, 'GA', 'Gabon');
+
+INSERT INTO `lm_zemlja` VALUES (79, 'GM', 'Gambia');
+
+INSERT INTO `lm_zemlja` VALUES (80, 'GE', 'Georgia');
+
+INSERT INTO `lm_zemlja` VALUES (81, 'DE', 'Germany');
+
+INSERT INTO `lm_zemlja` VALUES (82, 'GH', 'Ghana');
+
+INSERT INTO `lm_zemlja` VALUES (83, 'GI', 'Gibraltar');
+
+INSERT INTO `lm_zemlja` VALUES (84, 'GR', 'Greece');
+
+INSERT INTO `lm_zemlja` VALUES (85, 'GL', 'Greenland');
+
+INSERT INTO `lm_zemlja` VALUES (86, 'GD', 'Grenada');
+
+INSERT INTO `lm_zemlja` VALUES (87, 'GP', 'Guadeloupe');
+
+INSERT INTO `lm_zemlja` VALUES (88, 'GU', 'Guam');
+
+INSERT INTO `lm_zemlja` VALUES (89, 'GT', 'Guatemala');
+
+INSERT INTO `lm_zemlja` VALUES (90, 'GN', 'Guinea');
+
+INSERT INTO `lm_zemlja` VALUES (91, 'GW', 'Guinea-Bissau');
+
+INSERT INTO `lm_zemlja` VALUES (92, 'GY', 'Guyana');
+
+INSERT INTO `lm_zemlja` VALUES (93, 'HT', 'Haiti');
+
+INSERT INTO `lm_zemlja` VALUES (94, 'HM', 'Heard and Mc Donald Islands');
+
+INSERT INTO `lm_zemlja` VALUES (95, 'HN', 'Honduras');
+
+INSERT INTO `lm_zemlja` VALUES (96, 'HK', 'Hong Kong');
+
+INSERT INTO `lm_zemlja` VALUES (97, 'HU', 'Hungary');
+
+INSERT INTO `lm_zemlja` VALUES (98, 'IS', 'Iceland');
+
+INSERT INTO `lm_zemlja` VALUES (99, 'IN', 'India');
+
+INSERT INTO `lm_zemlja` VALUES (100, 'ID', 'Indonesia');
+
+INSERT INTO `lm_zemlja` VALUES (101, 'IR', 'Iran (Islamic Republic of)');
+
+INSERT INTO `lm_zemlja` VALUES (102, 'IQ', 'Iraq');
+
+INSERT INTO `lm_zemlja` VALUES (103, 'IE', 'Ireland');
+
+INSERT INTO `lm_zemlja` VALUES (104, 'IL', 'Israel');
+
+INSERT INTO `lm_zemlja` VALUES (105, 'IT', 'Italy');
+
+INSERT INTO `lm_zemlja` VALUES (106, 'CI', 'Ivory Coast');
+
+INSERT INTO `lm_zemlja` VALUES (107, 'JM', 'Jamaica');
+
+INSERT INTO `lm_zemlja` VALUES (108, 'JP', 'Japan');
+
+INSERT INTO `lm_zemlja` VALUES (109, 'JO', 'Jordan');
+
+INSERT INTO `lm_zemlja` VALUES (110, 'KZ', 'Kazakhstan');
+
+INSERT INTO `lm_zemlja` VALUES (111, 'KE', 'Kenya');
+
+INSERT INTO `lm_zemlja` VALUES (112, 'KI', 'Kiribati');
+
+INSERT INTO `lm_zemlja` VALUES (113, 'KP', 'Korea, Democratic People''s Republic of');
+
+INSERT INTO `lm_zemlja` VALUES (114, 'KR', 'Korea, Republic of');
+
+INSERT INTO `lm_zemlja` VALUES (115, 'KW', 'Kuwait');
+
+INSERT INTO `lm_zemlja` VALUES (116, 'KG', 'Kyrgyzstan');
+
+INSERT INTO `lm_zemlja` VALUES (117, 'LA', 'Lao People''s Democratic Republic');
+
+INSERT INTO `lm_zemlja` VALUES (118, 'LV', 'Latvia');
+
+INSERT INTO `lm_zemlja` VALUES (119, 'LB', 'Lebanon');
+
+INSERT INTO `lm_zemlja` VALUES (120, 'LS', 'Lesotho');
+
+INSERT INTO `lm_zemlja` VALUES (121, 'LR', 'Liberia');
+
+INSERT INTO `lm_zemlja` VALUES (122, 'LY', 'Libyan Arab Jamahiriya');
+
+INSERT INTO `lm_zemlja` VALUES (123, 'LI', 'Liechtenstein');
+
+INSERT INTO `lm_zemlja` VALUES (124, 'LT', 'Lithuania');
+
+INSERT INTO `lm_zemlja` VALUES (125, 'LU', 'Luxembourg');
+
+INSERT INTO `lm_zemlja` VALUES (126, 'MO', 'Macau');
+
+INSERT INTO `lm_zemlja` VALUES (127, 'MK', 'Macedonia');
+
+INSERT INTO `lm_zemlja` VALUES (128, 'MG', 'Madagascar');
+
+INSERT INTO `lm_zemlja` VALUES (129, 'MW', 'Malawi');
+
+INSERT INTO `lm_zemlja` VALUES (130, 'MY', 'Malaysia');
+
+INSERT INTO `lm_zemlja` VALUES (131, 'MV', 'Maldives');
+
+INSERT INTO `lm_zemlja` VALUES (132, 'ML', 'Mali');
+
+INSERT INTO `lm_zemlja` VALUES (133, 'MT', 'Malta');
+
+INSERT INTO `lm_zemlja` VALUES (134, 'MH', 'Marshall Islands');
+
+INSERT INTO `lm_zemlja` VALUES (135, 'MQ', 'Martinique');
+
+INSERT INTO `lm_zemlja` VALUES (136, 'MR', 'Mauritania');
+
+INSERT INTO `lm_zemlja` VALUES (137, 'MU', 'Mauritius');
+
+INSERT INTO `lm_zemlja` VALUES (138, 'TY', 'Mayotte');
+
+INSERT INTO `lm_zemlja` VALUES (139, 'MX', 'Mexico');
+
+INSERT INTO `lm_zemlja` VALUES (140, 'FM', 'Micronesia, Federated States of');
+
+INSERT INTO `lm_zemlja` VALUES (141, 'MD', 'Moldova, Republic of');
+
+INSERT INTO `lm_zemlja` VALUES (142, 'MC', 'Monaco');
+
+INSERT INTO `lm_zemlja` VALUES (143, 'MN', 'Mongolia');
+
+INSERT INTO `lm_zemlja` VALUES (144, 'MS', 'Montserrat');
+
+INSERT INTO `lm_zemlja` VALUES (145, 'MA', 'Morocco');
+
+INSERT INTO `lm_zemlja` VALUES (146, 'MZ', 'Mozambique');
+
+INSERT INTO `lm_zemlja` VALUES (147, 'MM', 'Myanmar');
+
+INSERT INTO `lm_zemlja` VALUES (148, 'NA', 'Namibia');
+
+INSERT INTO `lm_zemlja` VALUES (149, 'NR', 'Nauru');
+
+INSERT INTO `lm_zemlja` VALUES (150, 'NP', 'Nepal');
+
+INSERT INTO `lm_zemlja` VALUES (151, 'NL', 'Netherlands');
+
+INSERT INTO `lm_zemlja` VALUES (152, 'AN', 'Netherlands Antilles');
+
+INSERT INTO `lm_zemlja` VALUES (153, 'NC', 'New Caledonia');
+
+INSERT INTO `lm_zemlja` VALUES (154, 'NZ', 'New Zealand');
+
+INSERT INTO `lm_zemlja` VALUES (155, 'NI', 'Nicaragua');
+
+INSERT INTO `lm_zemlja` VALUES (156, 'NE', 'Niger');
+
+INSERT INTO `lm_zemlja` VALUES (157, 'NG', 'Nigeria');
+
+INSERT INTO `lm_zemlja` VALUES (158, 'NU', 'Niue');
+
+INSERT INTO `lm_zemlja` VALUES (159, 'NF', 'Norfork Island');
+
+INSERT INTO `lm_zemlja` VALUES (160, 'MP', 'Northern Mariana Islands');
+
+INSERT INTO `lm_zemlja` VALUES (161, 'NO', 'Norway');
+
+INSERT INTO `lm_zemlja` VALUES (162, 'OM', 'Oman');
+
+INSERT INTO `lm_zemlja` VALUES (163, 'PK', 'Pakistan');
+
+INSERT INTO `lm_zemlja` VALUES (164, 'PW', 'Palau');
+
+INSERT INTO `lm_zemlja` VALUES (165, 'PA', 'Panama');
+
+INSERT INTO `lm_zemlja` VALUES (166, 'PG', 'Papua New Guinea');
+
+INSERT INTO `lm_zemlja` VALUES (167, 'PY', 'Paraguay');
+
+INSERT INTO `lm_zemlja` VALUES (168, 'PE', 'Peru');
+
+INSERT INTO `lm_zemlja` VALUES (169, 'PH', 'Philippines');
+
+INSERT INTO `lm_zemlja` VALUES (170, 'PN', 'Pitcairn');
+
+INSERT INTO `lm_zemlja` VALUES (171, 'PL', 'Poland');
+
+INSERT INTO `lm_zemlja` VALUES (172, 'PT', 'Portugal');
+
+INSERT INTO `lm_zemlja` VALUES (173, 'PR', 'Puerto Rico');
+
+INSERT INTO `lm_zemlja` VALUES (174, 'QA', 'Qatar');
+
+INSERT INTO `lm_zemlja` VALUES (175, 'RE', 'Reunion');
+
+INSERT INTO `lm_zemlja` VALUES (176, 'RO', 'Romania');
+
+INSERT INTO `lm_zemlja` VALUES (177, 'RU', 'Russian Federation');
+
+INSERT INTO `lm_zemlja` VALUES (178, 'RW', 'Rwanda');
+
+INSERT INTO `lm_zemlja` VALUES (179, 'KN', 'Saint Kitts and Nevis');
+
+INSERT INTO `lm_zemlja` VALUES (180, 'LC', 'Saint Lucia');
+
+INSERT INTO `lm_zemlja` VALUES (181, 'VC', 'Saint Vincent and the Grenadines');
+
+INSERT INTO `lm_zemlja` VALUES (182, 'WS', 'Samoa');
+
+INSERT INTO `lm_zemlja` VALUES (183, 'SM', 'San Marino');
+
+INSERT INTO `lm_zemlja` VALUES (184, 'ST', 'Sao Tome and Principe');
+
+INSERT INTO `lm_zemlja` VALUES (185, 'SA', 'Saudi Arabia');
+
+INSERT INTO `lm_zemlja` VALUES (186, 'SN', 'Senegal');
+
+INSERT INTO `lm_zemlja` VALUES (187, 'SC', 'Seychelles');
+
+INSERT INTO `lm_zemlja` VALUES (188, 'SL', 'Sierra Leone');
+
+INSERT INTO `lm_zemlja` VALUES (189, 'SG', 'Singapore');
+
+INSERT INTO `lm_zemlja` VALUES (190, 'SK', 'Slovakia');
+
+INSERT INTO `lm_zemlja` VALUES (191, 'SI', 'Slovenia');
+
+INSERT INTO `lm_zemlja` VALUES (192, 'SB', 'Solomon Islands');
+
+INSERT INTO `lm_zemlja` VALUES (193, 'SO', 'Somalia');
+
+INSERT INTO `lm_zemlja` VALUES (194, 'ZA', 'South Africa');
+
+INSERT INTO `lm_zemlja` VALUES (195, 'GS', 'South Georgia South Sandwich Islands');
+
+INSERT INTO `lm_zemlja` VALUES (196, 'ES', 'Spain');
+
+INSERT INTO `lm_zemlja` VALUES (197, 'LK', 'Sri Lanka');
+
+INSERT INTO `lm_zemlja` VALUES (198, 'SH', 'St. Helena');
+
+INSERT INTO `lm_zemlja` VALUES (199, 'PM', 'St. Pierre and Miquelon');
+
+INSERT INTO `lm_zemlja` VALUES (200, 'SD', 'Sudan');
+
+INSERT INTO `lm_zemlja` VALUES (201, 'SR', 'Suriname');
+
+INSERT INTO `lm_zemlja` VALUES (202, 'SJ', 'Svalbarn and Jan Mayen Islands');
+
+INSERT INTO `lm_zemlja` VALUES (203, 'SZ', 'Swaziland');
+
+INSERT INTO `lm_zemlja` VALUES (204, 'SE', 'Sweden');
+
+INSERT INTO `lm_zemlja` VALUES (205, 'CH', 'Switzerland');
+
+INSERT INTO `lm_zemlja` VALUES (206, 'SY', 'Syrian Arab Republic');
+
+INSERT INTO `lm_zemlja` VALUES (207, 'TW', 'Taiwan');
+
+INSERT INTO `lm_zemlja` VALUES (208, 'TJ', 'Tajikistan');
+
+INSERT INTO `lm_zemlja` VALUES (209, 'TZ', 'Tanzania, United Republic of');
+
+INSERT INTO `lm_zemlja` VALUES (210, 'TH', 'Thailand');
+
+INSERT INTO `lm_zemlja` VALUES (211, 'TG', 'Togo');
+
+INSERT INTO `lm_zemlja` VALUES (212, 'TK', 'Tokelau');
+
+INSERT INTO `lm_zemlja` VALUES (213, 'TO', 'Tonga');
+
+INSERT INTO `lm_zemlja` VALUES (214, 'TT', 'Trinidad and Tobago');
+
+INSERT INTO `lm_zemlja` VALUES (215, 'TN', 'Tunisia');
+
+INSERT INTO `lm_zemlja` VALUES (216, 'TR', 'Turkey');
+
+INSERT INTO `lm_zemlja` VALUES (217, 'TM', 'Turkmenistan');
+
+INSERT INTO `lm_zemlja` VALUES (218, 'TC', 'Turks and Caicos Islands');
+
+INSERT INTO `lm_zemlja` VALUES (219, 'TV', 'Tuvalu');
+
+INSERT INTO `lm_zemlja` VALUES (220, 'UG', 'Uganda');
+
+INSERT INTO `lm_zemlja` VALUES (221, 'UA', 'Ukraine');
+
+INSERT INTO `lm_zemlja` VALUES (222, 'AE', 'United Arab Emirates');
+
+INSERT INTO `lm_zemlja` VALUES (223, 'GB', 'United Kingdom');
+
+INSERT INTO `lm_zemlja` VALUES (224, 'UM', 'United States minor outlying islands');
+
+INSERT INTO `lm_zemlja` VALUES (225, 'UY', 'Uruguay');
+
+INSERT INTO `lm_zemlja` VALUES (226, 'UZ', 'Uzbekistan');
+
+INSERT INTO `lm_zemlja` VALUES (227, 'VU', 'Vanuatu');
+
+INSERT INTO `lm_zemlja` VALUES (228, 'VA', 'Vatican City State');
+
+INSERT INTO `lm_zemlja` VALUES (229, 'VE', 'Venezuela');
+
+INSERT INTO `lm_zemlja` VALUES (230, 'VN', 'Vietnam');
+
+INSERT INTO `lm_zemlja` VALUES (231, 'VG', 'Virigan Islands (British)');
+
+INSERT INTO `lm_zemlja` VALUES (232, 'VI', 'Virgin Islands (U.S.)');
+
+INSERT INTO `lm_zemlja` VALUES (233, 'WF', 'Wallis and Futuna Islands');
+
+INSERT INTO `lm_zemlja` VALUES (234, 'EH', 'Western Sahara');
+
+INSERT INTO `lm_zemlja` VALUES (235, 'YE', 'Yemen');
+
+INSERT INTO `lm_zemlja` VALUES (236, 'YU', 'Yugoslavia');
+
+INSERT INTO `lm_zemlja` VALUES (237, 'ZR', 'Zaire');
+
+INSERT INTO `lm_zemlja` VALUES (238, 'ZM', 'Zambia');
+
+INSERT INTO `lm_zemlja` VALUES (239, 'ZW', 'Zimbabwe');
