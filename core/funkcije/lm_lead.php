427897c5ac2663c9952650cc26b04a13b13e5754
ssusta20 <ssusta20@vm-0.ssusta20.koding.kd.io>
2014-03-27 07:00:12 +0000

    funkcije za crud oprecije na tabelom lm_lead

--- core/funkcije/lm_lead.php
+++ core/funkcije/lm_lead.php
@@ -1,8 +1,35 @@
 <?php
 
-/*function registracija($ime, $prezime, $email, $password, $username){
- mysql_query("INSERT INTO lm_user (ime, prezime, email, password, username) VALUES ('$ime', '$prezime', '$email', '$password', '$username')");
-
-}*/
+    function lm_lead_insert ($sifra, $ime, $prezime, $email, $naziv_tvrtke, $telefon, $mobitel, $ulica, $grad, $zip, $napomena, $lm_user_id, $lm_sif_kvalif_id, $lm_zemlja_id){
+     mysql_query(
+     "INSERT INTO lm_lead (sifra, ime, prezime, email, naziv_tvrtke, telefon, mobitel, ulica, grad, zip, napomena, lm_user_id, lm_sif_kvalif_id, lm_zemlja_id)
+                    VALUES('$sifra', '$ime', '$prezime', '$email', '$naziv_tvrtke', '$telefon', '$mobitel', '$ulica', '$grad', '$zip', '$napomena', '$lm_user_id', '$lm_sif_kvalif_id', '$lm_zemlja_id')");
+    
+    }
+    
+     function lm_lead_update ($id,$sifra, $ime, $prezime, $email, $naziv_tvrtke, $telefon, $mobitel, $ulica, $grad, $zip, $napomena, $lm_user_id, $lm_sif_kvalif_id, $lm_zemlja_id){
+     mysql_query("UPDATE lm_lead 
+                  SET sifra = $sifra,
+                      ime = $ime,
+                      prezime = $prezime,
+                      email = $email,
+                      naziv_tvrtke = $naziv_tvrtke,
+                      telefon = $telefon,
+                      mobitel = $mobitel,
+                      ulica = $ulica,
+                      grad = $grad,
+                      zip = $zip,
+                      napomena = $napomena,
+                      lm_user_id = $lm_user_id,
+                      lm_sif_kvalif_id = $lm_sif_kvalif_id,
+                      lm_zemlja_id = $lm_zemlja_id
+                 WHERE id = '$id'");
+    
+    }
+    
+    function lm_lead_delete ($id){
+     mysql_query("DELETE FROM lm_lead WHERE id = '$id'");
+    
+    }
 ?>
 
