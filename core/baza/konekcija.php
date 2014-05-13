00bb5edf1c08a56a72e118a204f24591f46db5c6
ssusta20 <ssusta20@vm-0.ssusta20.koding.kd.io>
2014-05-13 07:02:46 +0000

    Dodan password za spajanje na hosting bazu

--- core/baza/konekcija.php
+++ core/baza/konekcija.php
@@ -1,7 +1,8 @@
 <?php
 mysql_connect('localhost','root','dpranjic') or die("Dogodila se greška prilikom spajanja na bazu, Oprostite!!!");
 mysql_select_db('lm') or die("Dogodila se greška prilikom spajanja na bazu, Oprostite!!!");
-#Konekcija za hosting bazu
-#mysql_connect('mysql.hostinger.hr','u763213958_root','dpranjic') or die("Dogodila se greška prilikom spajanja na bazu, Oprostite!!!");
-#mysql_select_db('u763213958_lm') or die("Dogodila se greška prilikom spajanja na bazu, Oprostite!!!");
+#Konekcija za 000webhost bazu
+#mysql_connect('mysql14.000webhost.com','a2983472_root','dpranjic123') or die("Dogodila se greška prilikom spajanja na bazu, Oprostite!!!");
+#mysql_select_db('a2983472_lm') or die("Dogodila se greška prilikom spajanja na bazu, Oprostite!!!");
+
 ?>
